79671cf0ec3fc911fece1dde409513d9aa0b9f46
Tianhao Li <ysihaoy@gmail.com>
2016-08-06 14:38:57 +1000

    owner ownee relationship and archive previous one

--- App.js
+++ App.js
@@ -4,24 +4,29 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      txt: 'this is the state txt',
-      cat: 0
-    }
+    this.state = { txt: '' }
+    this.update = this.update.bind(this)
   }
   update(e) {
-    console.log(e)
-    console.log(e.target);
     this.setState({txt: e.target.value})
   }
   render() {
     return (
       <div>
-        <input type="text" onChange={this.update.bind(this)} />
-        <h1>{this.state.txt}</h1>
+      <Widget txt={this.state.txt} update={this.update} />
+      <Widget txt={this.state.txt} update={this.update} />
       </div>
-    )
+    );
   }
+}
+
+const Widget = (props) => {
+  return (
+    <div>
+      <input type="text" onChange={props.update} />
+      <h1>{props.txt}</h1>
+    </div>
+  );
 }
 
 export default App
---
+++ App1.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      txt: 'this is the state txt',
+      cat: 0
+    }
+  }
+  update(e) {
+    console.log(e)
+    console.log(e.target);
+    this.setState({txt: e.target.value})
+  }
+  render() {
+    return (
+      <div>
+        <input type="text" onChange={this.update.bind(this)} />
+        <h1>{this.state.txt}</h1>
+      </div>
+    )
+  }
+}
+
+export default App
